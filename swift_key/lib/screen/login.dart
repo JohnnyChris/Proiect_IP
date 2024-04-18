467985a332d25d4a9f754add31e88de9b1b84d9e
JohnnyChris <87522704+JohnnyChris@users.noreply.github.com>
2024-04-18 13:42:20 +0300

    added logo and fixed bug

--- swift_key/lib/screen/login.dart
+++ swift_key/lib/screen/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 //import 'package:swift_key/screen/home.dart';
 
@@ -11,85 +12,93 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "data",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Text(
-            "Login to your app",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 44.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(
-            height: 44.0,
-          ),
-          //email box
-          const TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              hintText: "use your email",
-              prefixIcon: Icon(
-                Icons.mail,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 26.0,
-          ),
-          //password box
-          const TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              hintText: "use password",
-              prefixIcon: Icon(
-                Icons.lock,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 12.0,
-          ),
-          const Text(
-            "Ai uitat parola?",
-            style: TextStyle(color: Colors.black),
-          ),
-          const SizedBox(
-            height: 88.0,
-          ),
-          Container(
-            width: double.infinity,
-            child: RawMaterialButton(
-              fillColor: const Color(0xFF0069FE),
-              elevation: 0.0,
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0)),
-              onPressed: () {},
-              child: const Text(
-                "Login",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                //color: Colors.indigo, // To see the difference between the image's original size and the frame
+                height: 100,
+                width: 205,
+
+                child: Image.asset(
+                  "assets/SwiftkeyA.png",
+                  fit: BoxFit.cover,
                 ),
               ),
-            ),
+              const Text(
+                "Login to your app",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 44.0,
+              ),
+              //email box
+              const TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  prefixIcon: Icon(
+                    Icons.mail,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 26.0,
+              ),
+              //password box
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              const Text(
+                "Forgot password?",
+                style: TextStyle(color: Colors.black),
+              ),
+              const SizedBox(
+                height: 88.0,
+              ),
+              GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    width: double.infinity,
+                    child: RawMaterialButton(
+                      fillColor: const Color(0xFF0069FE),
+                      elevation: 0.0,
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0)),
+                      onPressed: () {},
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  ))
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
