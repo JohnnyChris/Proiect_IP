aa718577a833611c524a6d36649b37ade5093694
VictorJude046 <164768834+VictorJude046@users.noreply.github.com>
2024-05-23 01:29:01 +0300

    Bomba

--- Backend/Api/Get_data.py
+++ Backend/Api/Get_data.py
@@ -1,5 +1,5 @@
 
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, HTTPException
 from Backend import get_session
 from ORM_models.Utilizatori import Utilizatori, Utilizatori_pydantic
 from ORM_models.Angajati import Angajati_pydantic,Angajati
@@ -9,18 +9,31 @@ router = APIRouter(prefix="/get_data", tags=["get"])
 
 app = FastAPI()
 
-@router.get("/test")
-def get(Password:Angajati_pydantic):
-    print(Password)
+
     
-@router.get("/test_Utilizatori")
-def get_utilizator(id:int):
+#Verificare id
+@router.get("/Username")
+def get_Username(username:str):
     with get_session()as Session:
-        utilizatori=Session.query(Utilizatori).where(Utilizatori.ID_Utilizator==id).first()
-    return utilizatori
-    
-@router.get("/Angajati")
-def get_angajat(id:int):
+        username_utilizator=Session.query(Utilizatori).where(Utilizatori.Username==username).first()
+        if not username_utilizator:
+            raise HTTPException(status_code=404, detail="User not found")
+    return username_utilizator
+
+#Verificare Parola
+@router.get("/Password")
+def get_Password(Pass:str):
     with get_session()as Session:
-        angajati=Session.query(Angajati).where(Angajati.ID_Angajat==id).first()
-    return angajati    
+       parola_utilizator=Session.query(Utilizatori).where(Utilizatori.Parola==Pass).first()
+       if not parola_utilizator:
+           raise HTTPException(status_code=404, detail="Invalid password") 
+    return parola_utilizator
+
+#Afisare informatii dupa id
+@router.get("/Informatii_Utilizator")
+def get_Informatii(id:int):
+    with get_session()as Session:
+        Informatii=Session.query(Angajati).where(Angajati.ID_Utilizator==id).first()
+        if not Informatii:
+            raise HTTPException(status_code=404, detail="User not found")
+    return Informatii
