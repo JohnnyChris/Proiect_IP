ce9d400050493ae987d26040dd891e095942d856
VictorJude046 <164768834+VictorJude046@users.noreply.github.com>
2024-05-23 17:44:15 +0300

    login get_data

--- Backend/Api/Post_data.py
+++ Backend/Api/Post_data.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter,FastAPI
+from fastapi import APIRouter, FastAPI, HTTPException,Depends
+from Backend import get_session
+from ORM_models.Utilizatori import Utilizatori, Utilizatori_pydantic
+from ORM_models.Angajati import Angajati_pydantic,Angajati
+from sqlalchemy.orm import Session
+from ORM_models.Utilizatori import Utilizatori
+
 
 router = APIRouter(prefix="/post_data", tags=["post"])
 app = FastAPI()
 
-@router.get("/test")
-def get():
-    print("hello")
+
