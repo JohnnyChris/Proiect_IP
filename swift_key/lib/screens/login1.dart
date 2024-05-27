eca0572549395dbe90cad919eac25c4eb92111ef
AlinRogojanJ <alin.rogojan@student.upt.ro>
2024-05-28 00:42:40 +0300

    port

--- swift_key/lib/screens/login1.dart
+++ swift_key/lib/screens/login1.dart
@@ -12,7 +12,7 @@ class LoginScreen1 extends StatefulWidget {
   @override
   State<LoginScreen1> createState() => _LoginScreen1State();
 }
- 
+
 class _LoginScreen1State extends State<LoginScreen1> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -24,7 +24,7 @@ class _LoginScreen1State extends State<LoginScreen1> {
     final password = _passwordController.text;
 
     final response = await http.post(
-      Uri.parse('http://192.168.1.140:8000/auth/token'),
+      Uri.parse('http://192.168.1.10:8000/auth/token'),
       headers: <String, String>{
         'Content-Type': 'application/x-www-form-urlencoded',
       },
@@ -195,4 +195,3 @@ class _LoginScreen1State extends State<LoginScreen1> {
     );
   }
 }
-
